fc1057686967e6c6b600a02f29fc0789649de4ff
Luis Olarte <l.olartec@uniandes.edu.co>
2025-11-19 06:57:53 -0500

    movie service

---
+++ src/app/services/movie.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  private apiUrl = 'http://157.253.205.147:8080/api/movies';
+
+  constructor(private http: HttpClient) {}
+
+  getMovies(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  getMovieDetail(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+}
